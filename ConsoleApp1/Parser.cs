610dec4e5a3a1f67f158cb974a2844692e3bdb87
Vasily7628 <mineok44@gmail.com>
2023-08-13 22:39:52 +0700

    13.08.2023

--- ConsoleApp1/Parser.cs
+++ ConsoleApp1/Parser.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
+using System.Web;
 using Xceed.Words.NET;
 
-public class WordToHtmlParser
+namespace ConsoleApp1;
+
+public class ConsoleApp1
 {
     public static string ConvertToHtml(string filePath)
     {
@@ -12,75 +14,81 @@ public class WordToHtmlParser
             string html = styleHead;
             string[] patterns = new string[]
             {
-                @"_{2,}",   // Паттерн для поиска подчеркиваний (более 2 символов)
-                @"^(?!_{2,}).+$"    // Паттерн для поиска строки, не содержащей подчеркивание
+                @"_{2,}", // Паттерн для поиска подчеркиваний (более 2 символов)
+                @"^(?!_{2,}).+$" // Паттерн для поиска строки, не содержащей подчеркивание
             };
-
+            
             foreach (var paragraph in doc.Paragraphs)
             {
                 string paragraphText = HtmlEncode(paragraph.Text);
-                string formattedHtml = "";
-                bool shouldAddBlock = false; // Флаг для определения, добавлять ли блок
-
-                foreach (var pattern in patterns)
+                string formattedHtml = " ";
+                if (paragraphText.Length > 10)
                 {
-                    try
-                    {
-                        Match match = Regex.Match(paragraphText, pattern);
-                        if (match.Success)
-                        {
-                            switch (pattern)
-                            {
-                                case @"_{2,}":
-                                    string replace = match.Value.Replace("_", ""); // Удаление подчеркиваний
-                                    formattedHtml +=
-                                        $"<table width=\"100%\">\r\n" +
-                                        $"  <tr>\r\n" +
-                                        $"    <td width=\"60%\">\r\n" +
-                                        $"    </td>\r\n" +
-                                        $"    <td class=\"under_line\" width=\"40%\">\r\n" +
-                                        $"      <span style=\"border-bottom: 3px solid white;\">{replace}</span><br></td>\r\n" +
-                                        $"  </tr>\r\n" +
-                                        $"</table>";
 
-                                    shouldAddBlock = true;
-                                    break;
-
-                                case @"^(?!_{2,}).+$":
-                                    formattedHtml +=
-                                        $"<table width=\"100%\">\r\n" +
-                                        $"  <tr>\r\n" +
-                                       
-                                        $"    <td width=\"100%\"\">{paragraphText}</td>\r\n" +
-                                        $"  </tr>\r\n" +
-                                        $"</table>";
-
-                                    shouldAddBlock = true;
-                                    break;
-                            }
-
-                            break; // Выход из цикла, так как найдено совпадение
-                        }
-                    }
-                    catch (RegexParseException ex)
-                    {
-                        Console.WriteLine("Ошибка в регулярном выражении: " + ex.Message);
+                    string[] lines = Regex.Split(paragraphText, @"\r\n");
+                    foreach (string line in lines) {
+                        Console.WriteLine(line);
                     }
                 }
+                
 
-                if (shouldAddBlock)
-                {
-                    html += formattedHtml;
-                }
+                //formattedHtml = formatHtml(patterns, paragraphText, formattedHtml, ref shouldAddBlock);
+                
+    
+                //html += paragraphText;
             }
+            
 
             html += "</body></html>";
             return html;
         }
     }
 
+    private static string formatHtml(string[] patterns, string paragraphText, string formattedHtml)
+    {
+        foreach (var pattern in patterns)
+        {
+            try
+            {
+                Match match = Regex.Match(paragraphText, pattern);
+
+                switch (pattern)
+                {
+                    case @"_{2,}":
+                        string replace = match.Value.Replace("_", ""); // Удаление подчеркиваний
+                        formattedHtml +=
+                            $"<table width=\"100%\">\r\n" +
+                            $"  <tr>\r\n" +
+                            $"    <td width=\"60%\">\r\n" +
+                            $"    </td>\r\n" +
+                            $"    <td class=\"under_line\" width=\"40%\">\r\n" +
+                            $"      <span style=\"border-bottom: 3px solid white;\">{replace}</span><br></td>\r\n" +
+                            $"  </tr>\r\n" +
+                            $"</table>";
+                        break;
+
+                    case @"^(?!_{2,}).+$":
+                        formattedHtml +=
+                            $"<table width=\"100%\">\r\n" +
+                            $"  <tr>\r\n" +
+                            $"    <td width=\"100%\"\">{paragraphText}</td>\r\n" +
+                            $"  </tr>\r\n" +
+                            $"</table>";
+
+                        break;
+                }
+            }
+            catch (RegexParseException ex)
+            {
+                Console.WriteLine("Ошибка в регулярном выражении: " + ex.Message);
+            }
+        }
+
+        return formattedHtml;
+    }
+
     private static string HtmlEncode(string input)
     {
-        return System.Web.HttpUtility.HtmlEncode(input);
+        return HttpUtility.HtmlEncode(input);
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿namespace ConsoleApp1;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         string wordFilePath = "new.docx";
-        string htmlOutput = WordToHtmlParser.ConvertToHtml(wordFilePath);
+        string htmlOutput = ConsoleApp1.ConvertToHtml(wordFilePath);
 
         Console.WriteLine(htmlOutput);
     }
